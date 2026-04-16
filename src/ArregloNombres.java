eda78b754d8479737e208db454beb048f421dd67
TechExplorer677 <albaabel46@gmail.com>
2026-04-16 10:55:33 -0600

    Creacion de clase estudios

--- src/ArregloNombres.java
+++ src/ArregloNombres.java
@@ -1,16 +1,18 @@
 import java.util.Scanner;
+import java.util.logging.Logger;
 
 public class ArregloNombres {
+    private static final Logger logger = Logger.getLogger(ArregloNombres.class.getName());
     public static void main(String[] args) {
         Scanner teclado=new Scanner(System.in);
         String [] nombres=new String[3];
         for (int i=0; i< nombres.length;i++){
-            System.out.println("Ingrese un nombre ");
+            logger.info("Ingrese un nombre ");
             nombres[i]=teclado.nextLine();
         }
         //barrido
         for (int i=0; i< nombres.length;i++) {
-            System.out.println(nombres[i]+" ");
+            logger.info(nombres[i]);
         }
     }
 }
---
+++ src/Estudio.java
@@ -0,0 +1,12 @@
+public class Estudio {
+    public static void main(String[] args) {
+        int numero=10;
+        if (numero>0){
+            System.out.println("Es un numero positivo");
+        }else if (numero<0){
+            System.out.println("Es un numero negativo");
+        }else {
+            System.out.println("Es el numero cero");
+        }
+    }
+}
